42b63ce7f9a053d2cf78532e40471865eb3e1ad9
Juanjo <sanchez.silos@gmail.com>
2015-02-18 21:31:14 +0100

    Initial commit

---
+++ src/Rental.java
@@ -0,0 +1,21 @@
+/**
+ * Created by juanjo on 18/2/15.
+ */
+public class Rental {
+
+	private Movie _movie;
+	private int _daysRented;
+
+	public Rental(Movie movie, int daysRented) {
+		_movie = movie;
+		_daysRented = daysRented;
+	}
+
+	public int getDaysRented() {
+		return _daysRented;
+	}
+
+	public Movie getMovie() {
+		return _movie;
+	}
+}
